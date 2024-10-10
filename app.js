cd4f85f5c6dd60d5759a0a8e12af7e4d17513179
Shlapakova <shlapakova.kristina@mail.ru>
2024-10-10 12:31:17 +0200

    unit:upload proejct

---
+++ app.js
@@ -0,0 +1,35 @@
+"use strict"
+const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
+const ballRadius = 10;
+
+let x = canvas.width / 2;
+let y = canvas.height - 30;
+let dx = 2;
+let dy = -2;
+
+const paddleHeight = 10;
+const paddleWidth = 75;
+
+let paddleX = (canvas.width - paddleWidth) / 2;
+let rightPressed = false;
+let leftPressed = false;
+
+const brickRowCount = 5;
+const brickColumnCount = 3;
+const brickWidth = 75;
+const brickHeight = 20;
+const brickPadding = 10;
+const brickOffsetTop = 30;
+const brickOffsetLeft = 30;
+
+let score = 0;
+let lives = 3;
+
+let bricks = [];
+
+
+document.getElementById("runButton").addEventListener("click", function () {
+  draw();
+  this.disabled = true;
+});
